1413ef5187125414b64d1abf56e838b8a2693603
Wendy <wendy.tsai1988@gmail.com>
2021-02-01 21:50:45 +0100

    adjustements on todoApp

--- todoApp.js
+++ todoApp.js
@@ -1,7 +1,7 @@
 const Todo = (function() {
 
   let generateUniqueId = (function() {
-    let todoId = 0;
+    let todoId = 100;
     return function() {
       todoId += 1;
       return todoId;
@@ -54,16 +54,22 @@ const TodoList = (function() {
       return todo;
     }
   }
+  
+  function findTodo(inputId) {
+    return todos.filter(todo => {
+      return todo.id === inputId; 
+    })[0];
+  }
 
   return {
   
     init(todoDatas) {
-      todoDatas.forEach(todoData => {
+      todoDatas.forEach((todoData, idx) => {
         let todo = create(todoData);
         if (todo) {
           todos.push(todo);
         } else {
-          console.log(`${todoData} is not valid`);
+          console.log(`todoDatas index ${idx} is not valid`);
         }
       })
     },
@@ -77,16 +83,20 @@ const TodoList = (function() {
       return result;
     },
 
-    findTodo(inputId) {
-      return todos.filter(todo => {
-        return todo.id === inputId; 
-      })[0];
+    ReturnFoundTodoCopy(inputId) {
+      let result = findTodo(inputId);
+
+      if (result) {
+        let newObj = Object.create(Todo.prototype);
+        return Object.assign(newObj, result);
+      }
     },
 
     addTodo(todo) {
-      // if findTodo(todoid), then it's a duplicate -- logs duplication 
-      if (this.findTodo(todo.id)) {
+      if (findTodo(todo.id)) {
         console.log(" it is a duplicated todo");
+      } else if (todo.notValid) {
+        console.log("invalid todoData. Not added to the collection")
       } else {
         todos.push(todo);
         console.log( `${todo.title} is added`);
@@ -94,8 +104,7 @@ const TodoList = (function() {
     },
 
     deleteTodo(inputId) {
-      // if !findTodo(todoid), then can not delete --- logs todo not in collection, no deletion
-      let todoForDeleting = this.findTodo(inputId);
+      let todoForDeleting = findTodo(inputId);
       if (todoForDeleting) {
         todos.splice(todos.indexOf(todoForDeleting), 1);
         console.log(`${todoForDeleting.title} is deleted`);
@@ -105,7 +114,7 @@ const TodoList = (function() {
     },
 
     updateTodo(inputId, todoInfo) {
-      let todoForUpdating = this.findTodo(inputId);
+      let todoForUpdating = findTodo(inputId);
       if (todoForUpdating) {
         let idx = todos.indexOf(todoForUpdating);
         Object.keys(todoInfo).forEach(key => {
@@ -115,8 +124,6 @@ const TodoList = (function() {
       } else {
         console.log("no mathcing todo found for updating");
       }
-      // if findTodo(todoid), then get idx of todo from collection
-      // return copy of the updated todo
     },
   }
 })();
@@ -137,14 +144,12 @@ const TodoManager = {
   },
 
   getAllTodosOfGivenTime(inputMonth, inputYear) {
-    // go through this.todos and check for matching month & year
     return this.getAllTodos().filter(todo => {
       return todo.isWithinMonthYear(inputMonth, inputYear);
     })
   },
 
   getAllCompletedTodosOfGivenTime(inputMonth, inputYear) {
-    // call getAllTodosOfGivenTime(month, year), then select the ones with completed === true
     return this.getAllCompletedTodos().filter(todo => {
       return todo.isWithinMonthYear(inputMonth, inputYear);
     })
@@ -153,40 +158,41 @@ const TodoManager = {
 
 
 
+/*
+// 1. testings for Todo constructor :
 
-// testings for todos :
-// create todo
-let todoData1 = {
+// 1-1. create todo - with valid inputs
+let todoData0 = {
   title: 'Buy Milk',
   month: '12',
   year: '2017',
   description: 'Milk for baby',
 };
 
-let todoData1_2 = {
+let todoData1 = {
   title: 'Buy drinks',
   month: '',
   year: '2020',
   description: 'drinks for me',
 };
 
-let todoData1_3 = {
-  title: '',
-  month: '',
-  year: '2020',
-  description: 'drinks for me',
-};
+let todo1 = new Todo(todoData0);
+let todo2 = new Todo(todoData1);
 
-//let todo1 = new Todo(todoData1);
-//let todo2 = new Todo(todoData1_2);
-//console.log(new Todo(todoData1_3));   // logs {notValid: true}
-//console.log(todo1 instanceof Todo); // logs true
-//console.log(todo1.id); // logs 1
-// console.log(todo2.id); // logs 2
-//console.log(todo1.completed); // logs false
-//console.log(todo1.year); // logs 2017
+console.log(todo1 instanceof Todo); // logs true
+console.log(todo1.id); // logs 101
+console.log(todo2.id); // logs 102
+console.log(todo1.completed); // logs false
+console.log(todo1.year); // logs 2017
 
-// validation of todoData:
+
+// 1-2. test isWithinMonthYear method:
+console.log(todo1.isWithinMonthYear('12', '2017')); // logs true
+console.log(todo2.isWithinMonthYear('', '2020'));  // logs true
+console.log(todo2.isWithinMonthYear('2', '2020'));  // logs false
+
+
+// 1-3. create todos - with invalid todoData inputs:
 // invalid title -- empty string
 let todoData2 = {
   title: '',
@@ -233,27 +239,24 @@ let todoData7 = {
 let todoData8 = {
   title: 'Buy milk',
   month: '',
-  year: '12we',
+  year: '12',
   description: 'Milk',
 };
 
 
-//console.log(new Todo(todoData2));  // {notValid: true}
-//console.log(new Todo(todoData3));  // {notValid: true}
-//console.log(new Todo(todoData4));  // {notValid: true}
-//console.log(new Todo(todoData5));  // {notValid: true}
-//console.log(new Todo(todoData6));  // {notValid: true}
-//console.log(new Todo(todoData7));  // {notValid: true}
-//console.log(new Todo(todoData8));  // {notValid: true}
-
-// test isWithinMonthYear method:
-//console.log(todo1.isWithinMonthYear('12', '2017')); // logs true
-//console.log(todo2.isWithinMonthYear('', '2020'));  // logs true
-//console.log(todo2.isWithinMonthYear('2', '2020'));  // logs false
+console.log(new Todo(todoData2));  // {notValid: true}
+console.log(new Todo(todoData3));  // {notValid: true}
+console.log(new Todo(todoData4));  // {notValid: true}
+console.log(new Todo(todoData5));  // {notValid: true}
+console.log(new Todo(todoData6));  // {notValid: true}
+console.log(new Todo(todoData7));  // {notValid: true}
+console.log(new Todo(todoData8));  // {notValid: true}
+*/
 
 
-// testing for todoList
-// create todo collection
+
+// 2. testing for TodoList:
+// 2-1. create todo collection
 let todoData9 = {
   title: 'Buy juice',
   month: '1',
@@ -263,91 +266,114 @@ let todoData9 = {
 
 let todoData10 = {
   title: 'Buy Apples',
-  month: '',
+  month: '1',
   year: '2017',
   description: 'An apple a day keeps the doctor away',
 };
 
 let todoData11 = {
   title: 'Buy chocolate',
-  month: '1',
-  year: '',
+  month: '2',
+  year: '2020',
   description: 'For the cheat day',
 };
 
 let todoData12 = {
   title: 'Buy Veggies',
-  month: '',
-  year: '',
+  month: '2',
+  year: '2020',
   description: 'For the daily fiber needs',
 };
 
+// invalid input
+let todoData13 = {
+  title: '',
+  month: '2',
+  year: '2020',
+  description: '',
+};
 
-let todoSet = [todoData9, todoData10, todoData11, todoData12];
-TodoList.init(todoSet);
-// testing if TodoList collection can be mutated outside 
+
+let todoSet = [todoData9, todoData10, todoData11, todoData12, todoData13];
+TodoList.init(todoSet);  // logs [object object] is not valid
+//console.log(TodoList.list());  // logs an array of 4 objs - 101 Buy juice, 102 Buy apples, 103 Buy chocolate, 104 Buy Veggies
+
+// 2-2. testing TodoList collection can not be mutated outside 
 let listCopy = TodoList.list();
 let listCopy2 = TodoList.list();
-//console.log(listCopy);  // logs array with 4 todo objs
+//console.log(listCopy);  // logs an array of 4 objs - 101 Buy juice, 102 Buy apples, 103 Buy chocolate, 104 Buy Veggies
 listCopy[0] = 'wer';
 //console.log(listCopy);   // logs array with 1 string and 3 objs
-//console.log(TodoList.list()); // logs array with 4 todo objs
-listCopy2[0].title = "don't buy milk";
-//console.log(listCopy2); // logs 4 todos with "don't buy milk"
-//console.log(TodoList.list())  // logs original 4 todo objs with "Buy Milk"
+//console.log(TodoList.list()); // logs an array of 4 objs - 101 Buy juice, 102 Buy apples, 103 Buy chocolate, 104 Buy Veggies
+listCopy2[0].title = "get Takeaway";
+//console.log(listCopy2); // llogs an array of 4 objs - 101 get Takeaway, 102 Buy apples, 103 Buy chocolate, 104 Buy Veggies
+//console.log(TodoList.list())  // logs an array of 4 objs - 101 Buy juice, 102 Buy apples, 103 Buy chocolate, 104 Buy Veggies
 
 
-// test findTodo method:
-//console.log(TodoList.findTodo(4)) //  logs obj id#4
-//TodoList.findTodo(16);   // logs "no matching todo found"
+// 2-3. test ReturnFoundTodoCopy method & original todo in collection can not be mutated:
+let todo104 = TodoList.ReturnFoundTodoCopy(104);
+//console.log(todo104) //  logs todo obj id 104 Buy Veggies
+// console.log(todo104.isWithinMonthYear('2', '2020'))  // logs true
+//console.log(TodoList.ReturnFoundTodoCopy(16));   // logs undefined
+todo104.title = "Don't buy veggies";
+//console.log(todo104); // logs todo obj id 104 Don't buy veggies
+//console.log(TodoList.ReturnFoundTodoCopy(104))  // logs todo obj id 104 Buy Veggies
 
 
-// test addTodo method:
-let todoData13 = {
+
+// 2-4. test addTodo method:
+let todoData14 = {
   title: 'Buy books',
-  month: '',
-  year: '',
+  month: '1',
+  year: '2017',
   description: 'boos to read',
 };
-let todo13 = new Todo(todoData13);
-TodoList.addTodo(todo13);  // logs "Buy books"
-//TodoList.addTodo(todo13);  // logs "It is a duplicated todo"
-//console.log(TodoList.list()); // logs 5 x todo objs
+// invalid data
+let todoData15 = {
+  title: '',
+  month: '1',
+  year: '2017',
+  description: '',
+};
+let todo14 = new Todo(todoData14);
+TodoList.addTodo(todo14);  // logs "Buy books is added"
+//TodoList.addTodo(todo14);  // logs "It is a duplicated todo"
+//TodoList.addTodo(new Todo(todoData15));  // logs "invalid todoData. Not added to the collection"
+//console.log(TodoList.list()); // logs an array of 5 x objs - 101 Buy juice, 102 Buy apples, 103 Buy chocolate, 104 Buy Veggies ,105 Buy books
 
-// test deleteTodo method
-//TodoList.deleteTodo(2); // logs "Buy Apples is deleted "
-//TodoList.deleteTodo(20) // logs no matching todo found for deletion
-//console.log(TodoList.list()); // logs 4 x todo objs
 
-// test for updateTodo method
-//console.log(TodoList.updateTodo(3, {title: "don't buy chocolate", completed: true })); // logs 1 obj in array
-//console.log(TodoList.updateTodo(4, {month: '12', completed: true}));  // logs 1 obj in array
+// 2-5. test deleteTodo method:
+TodoList.deleteTodo(105); // logs "Buy books is deleted "
+//TodoList.deleteTodo(200) // logs no matching todo found for deletion
+//console.log(TodoList.list()); // logs an array of 4 x objs - 101 Buy juice, 102 Buy apples, 103 Buy chocolate, 104 Buy Veggies
+
+// test for updateTodo method && todo collection can not be mutated via updateTodo return value
+console.log(TodoList.updateTodo(101, {title: "don't buy juice", completed: true })); // logs an array of 1 obj - 101 don't buy juice, completed: true
+let updatedTodo = TodoList.updateTodo(102, {completed: true});  // return 1 obj in array - 102 buy apples completed: true
+updatedTodo[0].title = "buy chips";
+//console.log(updatedTodo); // return 1 obj in array - 102 buy chips completed: true
 //TodoList.updateTodo(80, {title: "change window"});  // logs no matching todo found for updating
-//console.log(TodoList.list());  // logs 4 x objs -- todos # id : 1, 3,4,5
+//console.log(TodoList.list());  // logs an array of 4 x objs - 101 don't buy juice & true, 102 Buy apples & true, 103 Buy chocolate & false, 104 Buy Veggies & false
 
 
-// testing for todoManager
-// getalltodo method
-
+// 3. testing for TodoManager:
+// 3-1. test getalltodo method and  todo collection can not be mutated by getalltodo return value:
 TodoManager.init(TodoList);
 let allTodos = TodoManager.getAllTodos();
-console.log(allTodos); // logs 4 x objs -- with buy juice todos # id : 1, 3,4,5
-allTodos[0].title = "don't buy juice";
-console.log(allTodos); // logs 3 x objs -- with "don't buy juice"
-console.log(TodoManager.getAllTodos()); // logs 4 x objs -- with buy juice
+//console.log(allTodos); // logs an array of 4 x objs - 101 don't buy juice & true, 102 Buy apples & true, 103 Buy chocolate & false, 104 Buy Veggies & false
+allTodos[0].title = "buy banana";
+//console.log(allTodos); // logs an array of 4 x objs - 101 buy banana & true, 102 Buy apples & true, 103 Buy chocolate & false, 104 Buy Veggies & false
+//console.log(TodoManager.getAllTodos()); // logs an array of 4 x objs - 101 don't buy juice & true, 102 Buy apples & true, 103 Buy chocolate & false, 104 Buy Veggies & false
 
-// getAllCompleteTodo method
-//console.log(TodoManager.getAllCompletedTodos()); // logs 2x objs -- #id #3 & #4
+// 3-2.  getAllCompleteTodo method:
+//console.log(TodoManager.getAllCompletedTodos()); // logs an array of 2 x objs - 101 don't buy juice & true, 102 Buy apples & true
 
-// getAllTodosOfGivenTime method
-//console.log(TodoManager.getAllTodosOfGivenTime('1', '')); // logs 1x obj -- # id 3
+// 3-3. getAllTodosOfGivenTime method:
+//console.log(TodoManager.getAllTodosOfGivenTime('1', '2017')); // logs an array of 2 x objs - 101 don't buy juice & true, 102 Buy apples & true
+//console.log(TodoManager.getAllTodosOfGivenTime('1', '2018')); // logs an empty array
+//console.log(TodoManager.getAllTodosOfGivenTime('2', '2020')); // logs an array of 2 x objs - 103 Buy chocolate & false, 104 Buy Veggies & false
 
-// getAllCompletedTodosOfGivenTime method
-//console.log(TodoManager.getAllCompletedTodosOfGivenTime("12","")); // logs 1x obj -- #id 4
+// 3-4. getAllCompletedTodosOfGivenTime method:
+//console.log(TodoManager.getAllCompletedTodosOfGivenTime("1","2017")); // logs an array of 2 x objs - 101 don't buy juice & true, 102 Buy apples & true
 //console.log(TodoManager.getAllCompletedTodosOfGivenTime('1', '2019')); // logs empty array
 
-/*
-let todoa = TodoList.findTodo(1);
-console.log(todoa);
-console.log(todoa.isWithinMonthYear('1', '2017'))
-*/
